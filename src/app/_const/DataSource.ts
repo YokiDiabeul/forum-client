271138511d640418f4f6270f00429e8becf686a3
YokiDiabeul <diabeul@live.fr>
2019-02-08 09:57:56 +0100

    reset

--- src/app/_const/DataSource.ts
+++ src/app/_const/DataSource.ts
@@ -1,2 +1,2 @@
-// export const url = 'http://localhost:8081/app/api';
 export const url = 'http://51.38.239.141:8080/forum-0.0.1-SNAPSHOT/api';
+// export const url = 'http://localhost:8081/app/api';
